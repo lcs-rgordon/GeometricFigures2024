182b05b47ec5082fd54d14d652a3dbb786a8f71e
Russell Gordon <rgordon@lcs.on.ca>
2024-09-30 05:55:25 -0400

    Initial project setup – added model (Circle code from earlier task).

---
+++ GeometricFigures/Views/CircleView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleView.swift
+//  GeometricFigures
+//
+//  Created by Russell Gordon on 2024-09-30.
+//
+
+import SwiftUI
+
+struct CircleView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleView()
+}
